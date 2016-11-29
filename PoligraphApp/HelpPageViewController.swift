e99ea20708ef8f7ab2983ae43c3705a19c9e4b8f
gblaketx <gordonblake@LearniOSFreecoms-Mac.local>
2016-11-28 20:39:33 -0800

    Added Storyboards - Not Yet Working

---
+++ PoligraphApp/HelpPageViewController.swift
@@ -0,0 +1,91 @@
+//
+//  HelpPageViewController.swift
+//  PoligraphApp
+//
+//  Created by Gordon Blake on 11/28/16.
+//  Copyright © 2016 Silo Busters. All rights reserved.
+//
+
+import UIKit
+
+class HelpPageViewController: UIPageViewController {
+
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newViewController(name: "Welcome 1"),
+                self.newViewController(name: "Welcome 2"),
+                self.newViewController(name: "Welcome 3")]
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        dataSource = self
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
+    }
+    
+    private func newViewController(name: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: name)
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+    // MARK: - UIPageViewControllerDataSource
+
+extension HelpPageViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+    
+}
