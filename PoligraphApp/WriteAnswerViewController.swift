8fc50adb24968c9b9976b99c5ef7317828c4430f
gblaketx <gordonblake@LearniOSFreecoms-Mac.local>
2016-12-03 02:16:44 -0800

    Write Answer Page Working
    Huzzah!
    
    Can successfully submit answer.

--- PoligraphApp/WriteAnswerViewController.swift
+++ PoligraphApp/WriteAnswerViewController.swift
@@ -21,12 +21,37 @@ class WriteAnswerViewController: UIViewController, UITextViewDelegate {
     @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
     @IBOutlet weak var summaryHeightConstraint: NSLayoutConstraint!
     
+    private struct Storyboard {
+        static let ShowAnswersSegueIdentifer = "Show Your Answers"
+    }
+    
     var question: Question? {
         didSet {
             updateUI()
         }
     }
     
+    @IBAction func submitButton(_ sender: UIBarButtonItem) {
+        let submitAnswerController = UIAlertController(title: "Submit Answer", message: "Are you sure you're ready to submit?", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+            //TODO: Add action
+        }
+        let submitAction = UIAlertAction(title: "Yes", style: .default) { (action) in
+            //TODO: Enforce content
+            if let question = self.question {
+                question.answer = self.answerTextView.text
+                question.summary = self.summaryTextView.text
+                question.status = Int32(Question.StatusTypes.answered.rawValue)
+                self.performSegue(withIdentifier: Storyboard.ShowAnswersSegueIdentifer, sender: sender)
+                if let user = (UIApplication.shared.delegate as! AppDelegate).currentUser {
+                    user.addToAnsweredQuestions(question)
+                }
+            }
+        }
+        submitAnswerController.addAction(cancelAction)
+        submitAnswerController.addAction(submitAction)
+        self.present(submitAnswerController, animated: true, completion: nil)
+    }
     var keyboardHeight: CGFloat?
     
     let characterLimit = 140
@@ -46,19 +71,13 @@ class WriteAnswerViewController: UIViewController, UITextViewDelegate {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidHide), name: Notification.Name.UIKeyboardDidHide, object: nil)
         
         summaryTextView.delegate = self
-        summaryTextView.clearsOnInsertion = true
         answerTextView.delegate = self
-        answerTextView.clearsOnInsertion = true
         
         let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         tap.numberOfTapsRequired = 1
-        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
-        //tap.cancelsTouchesInView = false
-        
         view.addGestureRecognizer(tap)
         
         updateUI()
-        // Do any additional setup after loading the view.
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -98,9 +117,12 @@ class WriteAnswerViewController: UIViewController, UITextViewDelegate {
     // MARK: - UI Text View Delegate Implementation
     
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-        let newText = (textView.text as NSString).replacingCharacters(in: range, with: text)
-        let numberOfChars = newText.characters.count
-        return numberOfChars <= characterLimit
+        if textView == summaryTextView {
+            let newText = (textView.text as NSString).replacingCharacters(in: range, with: text)
+            let numberOfChars = newText.characters.count
+            return numberOfChars <= characterLimit
+        }
+        return true
     }
     
     func textViewDidChange(_ textView: UITextView) {
@@ -131,14 +153,22 @@ class WriteAnswerViewController: UIViewController, UITextViewDelegate {
         
     }
     
-    /*
+
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+
+        if segue.identifier! == Storyboard.ShowAnswersSegueIdentifer {
+            if let tabBarController = segue.destination as? UITabBarController {
+                tabBarController.selectedIndex = 2
+                if let navController = tabBarController.selectedViewController as? UINavigationController {
+                    print("So far, so good")
+                    if let answersTVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Your Answers") as? YourAnswersTableViewController {
+                        navController.pushViewController(answersTVC, animated: false)
+                    }
+                }
+            }
+        }
     }
-    */
 
 }
